c16c8c187c973ba08c204d872a5cbeb2c237a382
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2026-04-29 12:01:31 +0200

    Make the crl script less odd looking by removing the trailing /

--- src/scripts/orca-protocol/sudoer/export_crls.nix
+++ src/scripts/orca-protocol/sudoer/export_crls.nix
@@ -9,16 +9,17 @@ in
   
   for PKI_NAME in $PKIS
   do
-      ISSUER_IDS=$(vault list -format=json ''${PKI_NAME}issuers | jq -r '.[]')
+      PKI_NAME=''${PKI_NAME%/}
+      ISSUER_IDS=$(vault list -format=json ''${PKI_NAME}/issuers | jq -r '.[]')
       for ISSUER_ID in $ISSUER_IDS
       do
-          if [ -n "$(vault read -format=json ''${PKI_NAME}issuer/$ISSUER_ID | jq -r 'select(.data.key_id != "")')" ]
+          if [ -n "$(vault read -format=json ''${PKI_NAME}/issuer/$ISSUER_ID | jq -r 'select(.data.key_id != "")')" ]
           then
-              PKI_AIA_DIR="${AIA_FOLDER}/''${PKI_NAME}issuer/$ISSUER_ID"
+              PKI_AIA_DIR="${AIA_FOLDER}/''${PKI_NAME}/issuer/$ISSUER_ID"
               CRL_DIR="$PKI_AIA_DIR/crl"
               mkdir -p $CRL_DIR
-              vault read -format=raw ''${PKI_NAME}issuer/$ISSUER_ID/der > $PKI_AIA_DIR/der
-              vault read -format=raw ''${PKI_NAME}issuer/$ISSUER_ID/crl/der > $CRL_DIR/der
+              vault read -format=raw ''${PKI_NAME}/issuer/$ISSUER_ID/der > $PKI_AIA_DIR/der
+              vault read -format=raw ''${PKI_NAME}/issuer/$ISSUER_ID/crl/der > $CRL_DIR/der
           fi
       done
   done
